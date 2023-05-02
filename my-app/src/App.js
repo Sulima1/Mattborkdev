c9aa3b7caf2246fe1d86f4611568e8e7bf6011f6
Matthew Borkowski <55469998+Sulima1@users.noreply.github.com>
2023-05-02 15:19:58 -0400

    Updates to structure

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from "react";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Navbar from "./components/Navbar.js";
+import Projects from "./components/Projects.js";
+
+export default function App() {
+  return (
+    <main className="text-[#C7D4F0] bg-[#262435] body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Contact />
+    </main>
+  );
+}
