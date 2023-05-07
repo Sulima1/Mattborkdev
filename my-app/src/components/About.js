a10af3927a7adc2dcb7c1f5e84bf9785315d0ea4
Matthew Borkowski <55469998+Sulima1@users.noreply.github.com>
2023-05-07 15:08:12 -0400

    Visual Updates

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,22 +1,34 @@
 // src/components/About.js
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function About() {
+  const [navbarHeight, setNavbarHeight] = useState(0);
+
+  useEffect(() => {
+    const navbar = document.querySelector("nav");
+    const height = navbar.offsetHeight;
+    setNavbarHeight(height);
+  }, []);
+
+  const sectionStyle = {
+    height: `calc(100vh - ${navbarHeight}px)`,
+  };
+
   return (
-    <section id="about" className="flex items-center justify-center h-screen">
+    <section id="about" className="flex items-center justify-center" style={sectionStyle}>
       <div className="container mx-auto flex px-5 py-20 flex-col items-center justify-center mt-[-150px]">
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 text-center">
-          <p className="no-wrap text-md md:text-lg lg:text-2xl mx-5 md:mx-8 mb-2 leading-relaxed text-[#D2931B]">
+          <p className="no-wrap text-md md:text-lg lg:text-2xl mx-5 md:mx-8 mb-2 leading-relaxed text-[#D2931B]" style={{ fontFamily: 'Inconsolata' }}>
             Hi, my name is
           </p>
-          <h1 className="no-wrap text-lg md:text-xl lg:text-4xl mx-5 md:mx-12 title-font sm:text-4xl text-3xl mb-4 font-medium text-[#C7D4F0]">
+          <h1 className="no-wrap text-lg md:text-xl lg:text-4xl mx-5 md:mx-12 title-font sm:text-4xl text-3xl mb-4 font-medium text-[#C7D4F0]" style={{ fontFamily: 'Inter' }}>
             Matthew Borkowski.
           </h1>
-          <h2 className="no-wrap text-lg md:text-xl lg:text-4xl mx-5 md:mx-16 title-font mb-4 font-medium text-[#C7D4F0]">
+          <h2 className="no-wrap text-lg md:text-xl lg:text-4xl mx-5 md:mx-16 title-font mb-4 font-medium text-[#C7D4F0]" style={{ fontFamily: 'Inter' }}>
             <span className="pl-4">Developer and student.</span>
           </h2>
-          <p className="mx-5 md:mx-8 mb-8 leading-relaxed">
+          <p className="mx-5 md:mx-8 mb-8 leading-relaxed" style={{ fontFamily: 'Inconsolata' }}>
             I'm a computer science student at <span className="text-[#D2931B]">Wilfrid Laurier University</span> <br className="hidden lg:inline-block" /> and I like to code things in my free time.
           </p>
         </div>
--- my-app/src/data.js
+++ my-app/src/data.js
@@ -5,16 +5,16 @@ export const projects = [
       title: "BoardMind",
       subtitle: "Python, PyTorch, Numpy, Pandas",
       description:
-        "Developed an artificial intelligence to analyze chess board positions accurately and make strategic decisions.",
-      image: "./project-1.gif",
+        "An artificial intelligence to analyze chess board positions accurately and make strategic decisions.",
+      image: "./project-1.jpg",
       link: "https://github.com/Sulima1/BoardMind-AI",
     },
     {
       title: "SafePulse",
       subtitle: "Python, Flask, React, Firebase, Twilio API",
       description:
-        "Developed an application to combat the opioid crisis across Canada, assisting those in need during the pandemic. First place project at HawkHacks 2022.",
-      image: "./project-2.gif",
+        "An application to combat the opioid crisis across Canada, assisting those in need during the pandemic. First place project at HawkHacks 2022.",
+      image: "./project-2.jpg",
       link: "https://devpost.com/software/safepulse",
     },
     {
@@ -22,7 +22,7 @@ export const projects = [
       subtitle: "Python, Flask, MongoDB, React, TailwindCSS",
       description:
         "An application built to simplify the student housing process for new renters, in a similar format as RateMyProfessor.",
-      image: "./project-3.gif",
+      image: "./media/MoxieChat_Preview.jpg",
       link: "https://devpost.com/software/ratemylandlord-oam5q4",
     },
     {
@@ -30,7 +30,7 @@ export const projects = [
       subtitle: "Go, Next, React, TailwindCSS, WebSocket API, Gin",
       description:
         "An open-source and anonymous chat application to help individuals learn new skills collaboratively with others.",
-      image: "./project-4.gif",
+      image: "./media/RateMyLandlord_Preview.jpg",
       link: "https://devpost.com/software/moxie",
     },
   ];
