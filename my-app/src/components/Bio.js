7953201aeaf09ba60d7ffe3745421bcd9dc87af4
Matthew Borkowski <55469998+Sulima1@users.noreply.github.com>
2025-03-30 11:03:33 -0400

    Update

--- my-app/src/components/Bio.js
+++ my-app/src/components/Bio.js
@@ -10,15 +10,12 @@ export default function MainApp() {
                 <img src={headshot} alt="headshot" className="mx-auto sm:mt-[-250px] md:mt-0 mr-10px relative rounded-full w-1/2"/>
             </div>
         <div className="w-full lg:w-1/2 lg:h-96 flex flex-col justify-center px-10 py-6">
-            <h1 className="lg:text-4xl font-bold mb-10 text-[#D2931B] text-center">
-            Some more about me.
-            </h1>
-            <p className="sm:text-xl lg:text-xl"style={{ fontFamily: 'Inconsolata' }}>
-              I am a BSc Computer Science graduate from Wilfrid Laurier University, in Waterloo, Ontario. My coding journey first began when I was 
-              pursuing a Bachelor of Design at the Ontario College for Art and Design. My courses involved creating digital art with an Arduino, from that experience I realized that I like programming more than I thought I would. That sent me down the coding rabbit hole and I am so glad I chose to do so. 
+            <p className="sm:text-sm lg:text-xl"style={{ fontFamily: 'Inconsolata' }}>
+              I am a BSc Computer Science graduate from Wilfrid Laurier University in Waterloo, Ontario. My coding journey first began when I was 
+              pursuing a Bachelor of Design at the Ontario College of Art & Design. My courses involved creating digital art with an Arduino. From that experience I realized that I enjoyed programming more than I thought I would. That started me on my coding journey and I am so glad. 
               <br></br><br></br>
-              Since my graduation, I've been focusing on my continuing my career in Software Engineering. It brings me so much joy and satisfaction to solve digital problems - they're like little puzzles or brain teasers. In my free time I like to collect records/cds, cook food from all around the world, and stay active in any way I can. 
-              If you want to contact me for work, projects, or even just to chat feel free to reach out to me!
+              Since my graduation, I've been focusing on continuing my career in Software Engineering. It brings me so much joy and satisfaction to solve digital problems - they're like little puzzles or brain teasers. In my free time I like to collect records/cds, cook food from all around the world, and continue pursuing my fitness goals. 
+              If you want to connect with me regarding work, projects, or even just to chat, contact me anytime.
             </p>
           </div>
         </div>
--- my-app/src/components/WorkHistory.js
+++ my-app/src/components/WorkHistory.js
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function WorkHistory() {
     return(
-        <section section id="bio" className="flex-direction: column">
+        <section section id="bio" className="flex-direction: column min-h-screen md:min-h-1200">
         {/* Left Section (Title & Text) */}
-        <div className="md:w-1/2 w-full ml-[10em] sm:text-center md:text-left md:p-8 lg:p-12">
+        <div className="md:w-1/2 w-full md:ml-[15em] sm:text-center md:text-left md:pt-12">
             <h1 className="text-2xl font-bold text-[#D2931B]"> Software Developer Contract</h1>
-            <p className="text-lg" style={{ fontFamily: "Inconsolata" }}>
+            <p className="fst-italic font-bold font-italic md:pb-1">Oshkosh Corporation</p>
+            <p className="text-md" style={{ fontFamily: "Inconsolata" }}>
             • Oversaw the deployment of C# and .NET projects to production in UiPath, managing dependencies, path mapping, and file
  transfers to ensure successful production code releases
  <br></br>• Diagnosed and resolved production Python and .NET code failures on production deployments within 24 hours, successfully
@@ -15,22 +16,34 @@ export default function WorkHistory() {
  AWS servers, and web applications using automation charts and ServiceNow, successfully resolving over 400 incidents
             </p>
         </div>
-        <div className="md:w-3/4 w-full ml-[10em] sm:text-center md:text-right md:p-8 lg:p-12">
+        <div className="md:end-10 md:w-1/3 w-full md:ml-[60em] sm:text-center md:text-left md:p-8">
             <h1 className="text-2xl font-bold text-[#D2931B]">Test</h1>
+            <p className="fst-italic font-bold font-italic md:pb-1">Oshkosh Corporation</p>
             <p className="text-lg" style={{ fontFamily: "Inconsolata" }}>
-            More job information
+            • Utilized Azure DevOps CI/CD Pipelines, managing 10+ user stories, and Agile methodologies to improve project delivery
+ and ensured accurate delivery with test driven development<br></br>• Developed automations integrating C#, .NET, Python, and REST APIs in scripts, reducing manual labor by 2,000 hours
+ <br></br>• Optimized automations using AI Computer Vision, Machine Learning, and OCR using UiPath and the .NET Stack, moving
+ projects through the SLDC and confirming success with user acceptance testing
+ <br></br>• Maintained and documented codebases with Git in the production GitHub repositories, using SharePoint and Azure for
+ seamless developer collaboration and documentation
             </p>
         </div>
-        <div className="md:w-3/4 w-full ml-[10em] sm:text-center md:text-left md:p-8 lg:p-12">
-            <h1 className="text-2xl font-bold text-[#D2931B]">Test</h1>
+        <div className="md:w-1/2 w-full md:ml-[15em] sm:text-center md:text-left md:p-8">
+            <h1 className="text-2xl font-bold text-[#D2931B]"> Software Developer Intern</h1>
+            <p className="fst-italic font-bold font-italic md:pb-1">Ziing Final Mile</p>
             <p className="text-lg" style={{ fontFamily: "Inconsolata" }}>
-            More job information
+            • Engineered ETL workflows using VBA to process and parse raw SQL datasets into structured financial reports in Excel, assisting with the financial data analysis of a rapidly growing startup
+ <br></br>• Conducted daily peer code reviews to ensure code quality, completeness, and security, in daily meetings with developers
+ enhancing production code practices and maintaining code standards across the team
             </p>
         </div>
-        <div className="md:w-3/4 w-full ml-[10em] sm:text-center md:text-right md:p-8 lg:p-12">
-            <h1 className="text-2xl font-bold text-[#D2931B]">Test</h1>
+        <div className="md:end-10 md:w-1/3 w-full md:ml-[60em] sm:text-center md:text-left md:pb-12">
+            <h1 className="text-2xl font-bold text-[#D2931B]"> Undergraduate Research</h1>
+            <p className="fst-italic font-bold font-italic md:pb-1">Wilfrid Laurier University</p>
             <p className="text-lg" style={{ fontFamily: "Inconsolata" }}>
-            More job information
+            • Utilized parallel programming techniques in C, including multi-threading to solve complex big data problems
+<br></br>• Developed efficient Python algorithms to process large datasets, enhancing data analysis capabilities
+<br></br>• Researched and implemented advanced algorithms for Hadamard matrix computation, optimizing performance and accuracy
             </p>
         </div>
 
